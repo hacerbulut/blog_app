695620cd63ab8f435c5ffe89d7724f4457fdb43a
hacer <hcrleather@gmail.com>
2023-04-04 00:42:52 +0300

    start

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,6 +4,7 @@ import NavBar from "../components/Navbar"
 import FooTer from "../components/FooTer"
 import About from "../pages/About"
 import NewBlog from "../pages/NewBlog"
+import Detail from "../pages/Detail"
 
 
 const AppRouter = () => {
@@ -14,6 +15,7 @@ const AppRouter = () => {
             <Route path="/" element={<Dashboard />} />
             <Route path="/about" element={<About />} />
             <Route path="/new-blog" element={<NewBlog />} />
+            <Route path="/detail/:id" element={<Detail />} />
         </Routes>
         <FooTer/>
     </Router>
